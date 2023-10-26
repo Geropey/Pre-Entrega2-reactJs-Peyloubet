8c46b5a409c8de79f4209dc5457d7cd595b37b10
Geropey <peyloubetbenjamin@gmail.com>
2023-10-26 01:26:39 -0300

    cambios2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { CartProvider } from './components/Context/Context';
 
 function App() {
 
@@ -11,15 +12,15 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
+      <CartProvider>
         <NavBar />
         <Routes>
-
           <Route path="/" element={<ItemListContainer greeting={'Bienvenidos'}/>} />
           <Route path="/category/:categoryId" element={<ItemListContainer greeting={'Bienvenidos'}/>} />
           <Route path="/detail/:detailId" element={<ItemDetailContainer/>} />
-
+          <Route path='/cart' element={<cart/>} />
         </Routes>
-
+        </CartProvider>
       </BrowserRouter>
 
     </div>
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,12 +1,18 @@
+import { useContext } from 'react'
 import cart from './Assets/cartt.png'
 import './CartWidget.css'
+import { CartContext } from '../Context/Context'
+import { Link } from 'react-router-dom'
 
 const CartWidget = () => {
+    const { totalQuantity } = useContext(CartContext)
     return (
+        <Link to='/cart' className='CartWidget' style={{ display: totalQuantity > 0 ? 'block' : 'none'}}> 
         <div className='cartt'>
             <img src={cart} alt="cart-widget"/>
-
+            {totalQuantity}
         </div>
+        </Link>
     )
 }
 
